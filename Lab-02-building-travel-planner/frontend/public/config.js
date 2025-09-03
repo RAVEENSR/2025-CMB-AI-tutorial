67af11302f597d5aa5752ea3aeb6cd0acb963377
Raveen Savinda Rathnayake <raveen.s.r@gmail.com>
2025-09-03 16:13:53 +0530

    Update files

---
+++ Lab-02-building-travel-planner/frontend/public/config.js
@@ -0,0 +1,8 @@
+// Configuration for WSO2 Choreo deployment
+// These values can be updated during deployment without rebuilding the React app
+
+window.REACT_APP_ASGARDEO_CLIENT_ID = "gN3C2joaMJxlesWx1KpGRejn8Oca";
+window.REACT_APP_ASGARDEO_BASE_URL = "https://api.asgardeo.io/t/raveen";
+window.REACT_APP_REDIRECT_SIGN_IN = window.location.origin;
+window.REACT_APP_REDIRECT_SIGN_OUT = window.location.origin;
+window.REACT_APP_HOTEL_API_BASE_URL = "http://localhost:9090";
